57dc7d95b1d7439471edb96f2c416fb43c81f514
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-12-01 12:32:22 -0500

    22

--- app/src/main.js
+++ app/src/main.js
@@ -27,47 +27,47 @@ const mangas = [
   {
     name: "One Piece",
     image: "OnePiece.jpg",
-    alt: "",
+    alt: "Adventures of Monkey D. Luffy, a young man with rubber-like abilities gained from eating a Devil Fruit, who sets out to find the legendary treasure",
   },
   {
     name: "Naruto",
     image: "Naruto.jpg",
-    alt: "",
+    alt: "The story of a young ninja, Naruto Uzumaki, who was ostracized as a child because a powerful demon fox was sealed inside him",
   },
   {
     name: "Bleach",
     image: "Bleach.png",
-    alt: "",
+    alt: "About a high school student, Ichigo Kurosaki, who gains the powers of a Soul Reaper (a death personification) after meeting Rukia Kuchiki",
   },
   {
     name: "Attack on Titan",
     image: "AttackOnTitan.png",
-    alt: "",
+    alt: "Where humanity lives in a walled city to protect themselves from giant, man-eating humanoids called Titans",
   },
   {
     name: "Jujutsu Kaisen",
     image: "JujutsuKaisen.jpg",
-    alt: "",
+    alt: "About a high school student named Yuji Itadori who becomes the host for a powerful demon named Ryomen Sukuna",
   },
   {
     name: "Bungo Stray Dogs",
     image: "BungoStrayDogs.webp",
-    alt: "",
+    alt: "About a group of detectives with supernatural abilities who solve cases too dangerous for the police, often clashing with the criminal Port Mafia",
   },
   {
     name: "Chainsaw Man",
     image: "ChainsawMan.jpg",
-    alt: "",
+    alt: "Denji, an impoverished teenager who makes a contract that fuses his body with that of Pochita, the dog-like Chainsaw Devil, granting him the ability to transform parts of his body into chainsaws",
   },
   {
     name: "Tokyo Ghoul",
     image: "TokyoGhoul.jpg",
-    alt: "",
+    alt: "Ken Kaneki, a college student who survives a ghoul attack only to become a half-ghoul himself after receiving ghoul organs in a transplant",
   },
   {
     name: "Spy x Family",
     image: "SpyXFamily.jpg",
-    alt: "",
+    alt: "A spy comedy about a master spy who creates a fake family to complete a mission, unaware that his wife is an assassin and his adopted daughter is a telepath",
   },
   {
     name: "Demon Slayer: Kimetsu no Yaiba",
@@ -88,7 +88,8 @@ const mangas = [
 
 function inject(item) {
   const container = document.querySelector(".container");
-  const html = ` <div class="card" data-name="${item.name}" data-img="${item.image}" data-alt="${item.alt}" data-price="${item.price}" >
+  const html = 
+    ` <div class="card" data-name="${item.name}" data-img="${item.image}" data-alt="${item.alt}" data-price="${item.price}" >
       <img class = "card-img" src="${item.image}">
       <h2 class = "card-name"> ${item.name}</h2>
       <p class = "card-alt"> ${item.alt}</p>
@@ -97,3 +98,5 @@ function inject(item) {
 }
 
 mangas.forEach((item) => inject(item));
+
+function 
