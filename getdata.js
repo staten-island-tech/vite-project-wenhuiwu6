b41b797e28e136c612d9dcc5f12ca2c35f8ba29a
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-12-11 12:31:22 -0500

    eeee

---
+++ getdata.js
@@ -0,0 +1,18 @@
+async function getData(poke) {
+  try {
+    //go get data
+    const response = await fetch("https:");
+    //handle error
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //makes the response into json data wer can use
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData("gengar");
